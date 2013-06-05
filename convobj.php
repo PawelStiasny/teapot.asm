962c69fe88725849974d14656a89bf0cbfc53d95
Paweł Stiasny <pawelstiasny@gmail.com>
2013-06-05 22:02:49 +0200

    Clean up the conversion script

--- convobj.php
+++ convobj.php
@@ -1,4 +1,5 @@
 <?php
+// convert .obj file to a list of lines
 
 $ar = file('teapot.obj');
 $scale = 400.0;
@@ -9,19 +10,17 @@ $pnum = 0;
 
 function put_line($vx1, $vx2) {
 	global $lines;
-	//echo $vx1[0] . ', ' . $vx1[1] . ', ' . $vx1[2] . ', ';
-	//echo $vx2[0] . ', ' . $vx2[1] . ', ' . $vx2[2] . ",\n";
 	$lines[] =  $vx1[0] . ' ' . $vx1[1] . ' ' . $vx1[2] . ' ' .
 		$vx2[0] . ' ' . $vx2[1] . ' ' . $vx2[2] . "\n";
 }
 
-// echo "float points[] = {\n";
-
 foreach ($ar as $line) {
 	$desc = explode(' ', $line);
 	if ($desc[0] == 'v') {
-		//$vxes[] = array( explode('/', $desc[1]), explode('/', $desc[2]), explode('/', $desc[3]) );
-		$vxes[] = array( (float)$desc[1] * $scale, (float)$desc[2] * $scale, (float)$desc[3] * $scale );
+		$vxes[] = array(
+			(float)$desc[1] * $scale,
+			(float)$desc[2] * $scale,
+			(float)$desc[3] * $scale );
 	} else if ($desc[0] == 'f') {
 		$face = array();
 		for ($i = 1; $i <= 3; $i++) {
@@ -35,9 +34,7 @@ foreach ($ar as $line) {
 	}
 }
 
-// echo "0 };\n";
 
-// echo "unsigned long num_points = $pnum;\n";
 echo count($lines) . "\n";
 foreach ($lines as $l) 
 	echo $l;
